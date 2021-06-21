a2b0301dccd636c35b5158a37f42bc620148f5dc
misha <mishakolyvanov@gmail.com>
2021-06-21 18:54:25 +0500

    Fix scanner.py

--- scanner.py
+++ scanner.py
@@ -13,13 +13,14 @@ def scan_port(ip: str, port: int) -> bool:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.settimeout(config.TIME)
     try:
-        sock.connect((ip, port))
-        log.debug(f'Port: {port} state: open')
-        sock.close()
-        return True
-    except socket.timeout:
-        log.debug(f'Port: {port} state: close')
-        return False
+        result = sock.connect_ex((ip, port))
+        if result == 0:
+            log.debug(f'Port: {port} state: open')
+            return True
+        else:
+            log.debug(f'Port: {port} state: close')
+            sock.close()
+            return False
     except socket.gaierror:
         log.debug(f'Не првильный ip {ip}')
         raise BadIpError
